1227783dd88cd0355354a10bf06bd230d978a9e2
Andrei Mouravski <amouravski@gmail.com>
2013-04-28 17:31:58 -0700

    Added dart2js to the build.dart.

--- finished/build.dart
+++ finished/build.dart
@@ -4,5 +4,12 @@ import 'package:web_ui/component_build.dart';
 import 'dart:io';
 
 void main() {
-  build(new Options().arguments, ['web/index.html']);
+  build(new Options().arguments, ['web/index.html'])
+    .then((results) {
+      print('Running dart2js on web_ui output...');
+      return Process.run('dart2js', [
+        results.first.bootstrapFile,
+      ]);
+    })
+    .then((_) => print('Build finished!'));
 }
