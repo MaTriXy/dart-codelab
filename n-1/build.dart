52a7373a1953f24b0aff3e68d4bc8b899114d99d
Andrei Mouravski <amouravski@gmail.com>
2013-04-30 15:04:42 -0700

    Simplified build.dart.

--- n-1/build.dart
+++ n-1/build.dart
@@ -2,18 +2,18 @@
 
 import 'package:web_ui/component_build.dart';
 import 'dart:io';
+import 'dart:async';
 
 void main() {
-  build(new Options().arguments, ['web/index.html'])
-    .then((Iterable results) {
-      print('Running dart2js on web_ui output...');
-      if (!results.isEmpty && results.first.bootstrapFile != null &&
-          new File(results.first.bootstrapFile).existsSync()) {
-        return Process.run('dart2js', [
-          results.first.bootstrapFile,
-          '--out=${results.first.bootstrapFile}.js'
-        ]);
-      }
-    })
+  var args = new List.from(new Options().arguments);
+  build(args, ['web/index.html'])
+    .then((_) => _runDart2js('web/out/index.html_bootstrap.dart'))
     .then((_) => print('Build finished!'));
+}
+
+Future<ProcessResult> _runDart2js(String file) {
+  if (new File(file).existsSync()) {
+    print('Running dart2js on ${file}...');
+    return Process.run('dart2js', [file, '--out=${file}.js']);
+  }
 }
