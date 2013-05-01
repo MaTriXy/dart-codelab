1680df4877cf4437e8009d3bedce68102bce97df
Andrei Mouravski <amouravski@gmail.com>
2013-05-01 12:37:33 -0700

    Completed refactoring and cleaning up code.

--- finished/lib/document.dart
+++ finished/lib/document.dart
@@ -1,14 +1,16 @@
+/**
+ * A library for the document object.
+ */
 library document;
 
 import 'dart:json' as JSON;
 import 'dart:math' show Random;
 
+import 'package:intl/intl.dart';
 import 'package:web_ui/web_ui.dart';
 
-
-
 /**
- * A text file.
+ * A file of plain text.
  *
  * The document keeps track of its creation and modification time.
  */
@@ -18,7 +20,10 @@ class Document {
   String _title;
   String _content;
 
+  /// Creation time.
   DateTime created;
+
+  /// Last modification time.
   DateTime modified;
 
   /// The unique identifier is `document-` followed by a large random number.
@@ -67,20 +72,26 @@ class Document {
     return '$count words';
   }
 
-
   // These setters and getters are used to update the modification time.
 
+  /// The title of our document.
   set title(String title) {
     _title = title;
     modified = new DateTime.now();
   }
-
   String get title => _title;
 
+  /// The next best-seller, maybe?
   set content(String content) {
     _content = content;
     modified = new DateTime.now();
   }
-
   String get content => _content;
 }
+
+/**
+ * Formatting for document dates.
+ *
+ * Parsed time looks like: 'April 29, 9:48 AM'
+ */
+DateFormat dateFormat = new DateFormat.MMMMd().addPattern('',', ').add_jm();
