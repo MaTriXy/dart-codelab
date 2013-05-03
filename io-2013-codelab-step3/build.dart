b6e0c2fe411ddf62d8f03dc3c4465eb821f41d11
Andrei Mouravski <amouravski@gmail.com>
2013-05-03 14:32:16 -0700

    Made build.dart nicer.

--- io-2013-codelab-step3/build.dart
+++ io-2013-codelab-step3/build.dart
@@ -7,13 +7,5 @@ import 'dart:async';
 void main() {
   var args = new List.from(new Options().arguments);
   build(args, ['web/index.html'])
-//    .then((_) => _runDart2js('web/out/index.html_bootstrap.dart'))
     .then((_) => print('Build finished!'));
-}
-
-Future<ProcessResult> _runDart2js(String file) {
-  if (new File(file).existsSync()) {
-    print('Running dart2js on ${file}...');
-    return Process.run('dart2js', [file, '--out=${file}.js']);
-  }
 }
